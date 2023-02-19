21d8bdaf0c8b50f5529f49c2e55da28a2b77f0e5
joycexjiang <jj3115@barnard.edu>
2023-02-18 22:32:30 -0800

    made the textarea editable

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 
 import Layout from "../components/Layout";
 
-// TODO: make the textarea editable.
-
 const Home: NextPage = () => {
 
   const meetings = [
@@ -32,10 +30,20 @@ const Home: NextPage = () => {
       <div className="container mx-auto">
         <div className="pt-10 lg:grid md:grid-cols-11 gap-16">
           <div className="col-span-4">
-            <div className="border-gray-300 border-[1px] rounded-xl py-8 px-10 text-md leading-relaxed bg-white opacity-70 mb-6">
-              Use this area to plan any premeditated questions, thoughts, or to
-              jot down some quick notes!
-            </div>
+           
+
+              <div className="mt-1">
+                    <textarea
+                      id="notepad"
+                      name="notepad"
+                      rows={5}
+                      className="min-h-80% mt-1 leading-relaxed block w-full rounded-md border-gray-300  border-[1px] shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-md py-8 px-10 mb-6"
+                      placeholder="Use this area to plan any premeditated questions, thoughts, or to
+                      jot down some quick notes!"
+                      defaultValue={''}
+                    />
+                  </div>
+
           </div>
           <div className="col-span-7">
             <h1 className="text-4xl mb-12">My meetings</h1>

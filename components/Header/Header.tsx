da21a121ad49866e9e2ff5d49a28c6cf7c82a300
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-02-18 19:54:44 -0800

    Created header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,47 @@
 import Link from "next/link";
 import logo from "../../assets/vercel.svg";
 import Image from "next/image";
+import { Cog8ToothIcon } from "@heroicons/react/24/outline";
 
 export default function Header() {
   return (
-    <div className="container mx-auto lg:px-36 px-6 ">
-      <div className="py-6">
-        <div className=" inline-block">
-          <Link href="/">
-            <Image src={logo} alt="Snapnotes logo" height={35} />
-          </Link>
+    <div className="px-6 py-6 border-b-2">
+      <div className="flex justify-between">
+        <div className="w-48">logo</div>
+        <div className="flex flex-grow justify-between gap-4">
+          <div className="flex-grow border-2 flex-row border-[#D9D9D9] bg-white py-2 rounded-xl text-sm ">
+            <input
+              type="text"
+              // value={"filterText"}
+              // onChange={(e) => setFilterText(e.target.value)}
+              placeholder="Search"
+              className="inline focus:outline-none ml-3 flex-1"
+            ></input>
+          </div>
+          <div>
+            <button
+              onClick={() => console.log("new meeting")}
+              className="border-2 border-[#D9D9D9] bg-white px-5 py-2 rounded-xl text-sm text-gray-700"
+            >
+              New Meeting
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => console.log("new meeting")}
+              className="border-2 border-[#D9D9D9] bg-white px-5 py-2 rounded-xl text-sm text-gray-700"
+            >
+              Upload Meeting
+            </button>
+          </div>
+          <div className="flex flex-col justify-center">
+            <button
+              onClick={() => console.log("new meeting")}
+              className="border-[1px] border-[#D9D9D9] bg-white px-2 py-2 rounded-full text-sm text-gray-700"
+            >
+              <Cog8ToothIcon className="h-6 w-6" />
+            </button>
+          </div>
         </div>
       </div>
     </div>

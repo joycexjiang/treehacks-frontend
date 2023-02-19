0c59734cdccec461fe39d2894c7ec669247e0e7c
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-02-19 09:55:37 -0800

    Update API URL

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const analyze = async (firebaseUrl: string, docId: string) => {
   const base64Encoded = Buffer.from(firebaseUrl).toString("base64");
-  const baseUrl = "http://34.150.158.198:8000/conversation_analysis";
+  const baseUrl = "http://34.150.158.198/conversation_analysis";
 
   const response = await axios.post(baseUrl, null, {
     params: { url: base64Encoded, docId },
@@ -12,7 +12,7 @@ export const analyze = async (firebaseUrl: string, docId: string) => {
 };
 
 export const search = async (query: string) => {
-  const baseUrl = "http://34.150.158.198:8000/search";
+  const baseUrl = "http://34.150.158.198/search";
 
   const response = await axios.post(baseUrl, null, {
     params: { query },
